15016503029c49ee82cf4689929e76a4e1b7f3c7
MaximusZhou <zhougang009@gmail.com>
2015-07-02 22:36:37 +0800

    add chapter one code

---
+++ Chapter1/daytimetcpsrv_1_2.c
@@ -0,0 +1,34 @@
+#include "../lib/unp.h"
+#include	<time.h>
+
+int
+main(int argc, char **argv)
+{
+	int					listenfd, connfd;
+	struct sockaddr_in	servaddr;
+	char				buff[MAXLINE];
+	time_t				ticks;
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd < 0)
+		err_sys("socket error");
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(13000);	/* daytime server */
+
+	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+
+	listen(listenfd, LISTENQ);
+
+	for ( ; ; ) {
+		connfd = accept(listenfd, (SA *) NULL, NULL);
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, buff, strlen(buff));
+
+		close(connfd);
+	}
+}
