8ad4ccf115383e2dfe1c6b84a2d330be415ff981
MaximusZhou <zhougang009@gmail.com>
2015-07-13 21:35:45 +0800

    check system byte order

---
+++ Chapter3/byteorder_3_10.c
@@ -0,0 +1,32 @@
+/*
+	Author: MaximusZhou
+	Example:
+	$gcc -Wall byteorder_3_10.c -o byteorder_3_10 
+	$./byteorder_3_10 
+	x86_64-unknown-linux-gnu: little-endian
+	Analyse:
+*/
+
+#include "../lib/unp.h"
+
+int main(int argc, char **argv)
+{
+	union {
+	  short  s;
+      char   c[sizeof(short)];
+    } un;
+
+	un.s = 0x0102;
+	printf("%s: ", CPU_VENDOR_OS);
+	if (sizeof(short) == 2) {
+		if (un.c[0] == 1 && un.c[1] == 2)
+			printf("big-endian\n");
+		else if (un.c[0] == 2 && un.c[1] == 1)
+			printf("little-endian\n");
+		else
+			printf("unknown\n");
+	} else
+		printf("sizeof(short) = %lu\n", sizeof(short));
+
+	exit(0);
+}
